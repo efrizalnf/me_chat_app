feb6e48055976df8be0a4ba75bdb33ec35638969
efrizalnf <ondabuzz9@gmail.com>
2023-06-03 09:33:57 +0700

    added sliverwidgetin in chat screen, but not yet fixed

--- lib/module/home/controller/home_controller.dart
+++ lib/module/home/controller/home_controller.dart
@@ -1,7 +1,11 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:me_chat_app/core.dart';
 
 import '../../../services/auth_service/firebase_chat_service.dart';
+import '../widget/chat_bubble_msg.dart';
+import '../widget/chat_input_msg.dart';
 
 class HomeController extends State<HomeView> implements MvcController {
   static late HomeController instance;
@@ -40,6 +44,81 @@ class HomeController extends State<HomeView> implements MvcController {
 
   Stream<dynamic> getMessageList() {
     return FirebaseChatServices.getMessageList();
+  }
+
+  SliverList getSlivers() {
+    return SliverList(
+      delegate: SliverChildListDelegate(
+        [
+          StreamBuilder(
+            stream: getMessageList(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                return const Center(
+                  child: Text('No Message Found'),
+                );
+              }
+              if (snapshot.hasError) {
+                return const Center(
+                  child: Text('Something wrong'),
+                );
+              }
+              final ScrollController controller = ScrollController();
+              final messages = snapshot.data!.docs;
+              return ScrollConfiguration(
+                behavior:
+                    ScrollConfiguration.of(context).copyWith(dragDevices: {
+                  PointerDeviceKind.touch,
+                  PointerDeviceKind.mouse,
+                }),
+                child: Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 16, right: 16),
+                    child: ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      controller: controller,
+                      reverse: true,
+                      itemCount: messages.length,
+                      itemBuilder: (ctx, index) {
+                        final chatMessage = messages[index].data();
+                        final nextChatMessage = index + 1 < messages.length
+                            ? messages[index + 1].data()
+                            : null;
+                        final currentMessageUserId = chatMessage['uid'];
+                        final nextMessageUserId = nextChatMessage != null
+                            ? nextChatMessage['uid']
+                            : null;
+                        final nextUserIsSame =
+                            nextMessageUserId == currentMessageUserId;
+                        if (nextUserIsSame) {
+                          return MessageBubble.next(
+                              message: chatMessage['message'],
+                              isMe: authUser!.uid == currentMessageUserId);
+                        } else {
+                          print(chatMessage['photo']);
+                          return MessageBubble.first(
+                              userImage: chatMessage['photo'],
+                              username: chatMessage['username'],
+                              message: chatMessage['message'],
+                              isMe: authUser!.uid == currentMessageUserId);
+                        }
+                      },
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 
   @override
--- lib/module/home/view/home_view.dart
+++ lib/module/home/view/home_view.dart
@@ -10,36 +10,170 @@ class HomeView extends StatefulWidget {
 
   Widget build(context, HomeController controller) {
     controller.view = this;
-
+    bool pinned = true;
+    bool snap = false;
+    bool floating = true;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Me Chat"),
-        actions: [
-          IconButton(
-            onPressed: controller.doLogout,
-            icon: const Icon(
-              Icons.exit_to_app,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.white,
+            leadingWidth: 0,
+            pinned: pinned,
+            snap: snap,
+            toolbarHeight: 100.0,
+            collapsedHeight: 100.0,
+            primary: false,
+            title: Padding(
+              padding: const EdgeInsets.only(top: 45.0, bottom: 15.0),
+              child: Row(
+                children: [
+                  const CircleAvatar(
+                    radius: 24.0,
+                    backgroundImage: NetworkImage(
+                      "https://i.ibb.co/PGv8ZzG/me.jpg",
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10.0,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "Johny Greenwood",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87,
+                          shadows: [
+                            Shadow(
+                              color: Colors.white,
+                              offset: Offset.zero,
+                              blurRadius: 10.0,
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5.0,
+                      ),
+                      Text(
+                        "Online",
+                        style: TextStyle(
+                          fontSize: 12.0,
+                          color: Colors.green,
+                          shadows: [
+                            Shadow(
+                              color: Colors.white,
+                              offset: Offset.zero,
+                              blurRadius: 4.0,
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 45.0, bottom: 15.0),
+                child: IconButton(
+                    icon: const Icon(Icons.more_vert), onPressed: () {}),
+              )
+            ],
+            floating: floating,
+            expandedHeight: 155,
+            flexibleSpace: Stack(children: [
+              Positioned.fill(
+                child: FlexibleSpaceBar(
+                  background: Image.network(
+                    "https://source.unsplash.com/qE5K_972f48",
+                    width: 64.0,
+                    height: 64.0,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: -7,
+                left: 0,
+                right: 0,
+                child: Container(
+                  // height: 33,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(40),
+                    ),
+                  ),
+                ),
+              )
+            ]),
+          ),
+          Expanded(
+            child: controller.getSlivers(),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ChatInputMsg(
+                  chatController: controller.chatController,
+                  isShowEmoji: controller.isShowEmoji,
+                  showEmoji: controller.showEmoji,
+                  onSendMessage: controller.sendMessage,
+                ),
+              ),
             ),
           )
         ],
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ChatListMsg(
-              stream: controller.getMessageList(),
-              userId: controller.authUser!,
-            ),
-          ),
-          ChatInputMsg(
-            chatController: controller.chatController,
-            isShowEmoji: !controller.isShowEmoji,
-            showEmoji: controller.showEmoji,
-            onSendMessage: controller.sendMessage,
-          ),
-        ],
-      ),
+      // bottomNavigationBar: Container(
+      //   width: double.maxFinite,
+      //   child: ChatInputMsg(
+      //     chatController: controller.chatController,
+      //     isShowEmoji: controller.isShowEmoji,
+      //     showEmoji: controller.showEmoji,
+      //     onSendMessage: controller.sendMessage,
+      //   ),
+      // ),
     );
+
+    // Scaffold(
+    //   appBar: AppBar(
+    //     title: const Text("Me Chat"),
+    //     actions: [
+    //       IconButton(
+    //         onPressed: controller.doLogout,
+    //         icon: const Icon(
+    //           Icons.exit_to_app,
+    //         ),
+    //       )
+    //     ],
+    //   ),
+    //   body: Column(
+    //     children: [
+    //       Expanded(
+    //         child: ChatListMsg(
+    //           stream: controller.getMessageList(),
+    //           userId: controller.authUser!,
+    //         ),
+    //       ),
+    //       ChatInputMsg(
+    //         chatController: controller.chatController,
+    //         isShowEmoji: !controller.isShowEmoji,
+    //         showEmoji: controller.showEmoji,
+    //         onSendMessage: controller.sendMessage,
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 
   @override
